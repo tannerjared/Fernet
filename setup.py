4785e3a57991e2009ddc168370d8b22796bbeda2
Jared Tanner <46349915+tannerjared@users.noreply.github.com>
2024-01-05 17:51:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-from distutils.core import setup
-from distutils.extension import Extension
-import numpy as np
+from setuptools import setup, find_packages
 import os
 
-setup(name='fernet',
+# This was updated by Jared Tanner, jjtanner@ufl.edu
+
+setup(
+    name='fernet',
     description='Single shell free-water correction for Diffusion Tensor Imaging data.',
     version='0.1.dev0',
     author='William Parker',
     author_email='William.Parker@uphs.upenn.edu',
     url='http://www.med.upenn.edu/sbia/',
-    scripts = [ os.path.join('scripts', 'fernet.py'),
-                os.path.join('scripts', 'fernet_regions.py'),
-                os.path.join('scripts', 'fernet_fw_dwi.py'),
-                ],
-    packages = ['fernet',
-                ],
-    )
+    scripts=[
+        os.path.join('scripts', 'fernet.py'),
+        os.path.join('scripts', 'fernet_regions.py'),
+        os.path.join('scripts', 'fernet_fw_dwi.py'),
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.10.4',
+        'scipy>=0.17.1',
+        'nibabel>=2.0.1',
+        'dipy>=0.10.1',
+    ],
+)
